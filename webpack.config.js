2f1386d98dd0f50d2cd8f4ff12196d0bd48ed971
shichang.wang <shichang.wang@qiaofang.info>
2019-07-23 17:36:34 +0800

    feat: hello webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
+const path = require('path');
+
 module.exports = {
-    entry:'',//入口文件
-    output:{},//出口文件
-    module:{},//处理对应模块
-    plugins:[],//对应插件
-    devServer:{},//开发服务器配置
-    mode:'development'//模式配置
-}
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
